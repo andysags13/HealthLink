e21691b7a3bbb276af797db6c8a19eddc36d0027
Andy <andy.sagbo@epitech.eu>
2024-05-12 19:54:22 +0100

    HealthLink

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleDocumentation() {
+    var documentation = document.getElementById("documentation");
+    var dropdownContent = document.getElementById("maladies-dropdown");
+    var message = document.querySelector(".centered-message");
+    
+    if (documentation.style.display === "none") {
+        documentation.style.display = "block";
+        dropdownContent.style.display = "block";
+        message.style.display = "none";
+    } else {
+        documentation.style.display = "none";
+        dropdownContent.style.display = "none";
+        message.style.display = "block";
+    }
+}
+
+function toggleAnalyse() {
+    var analyse = document.getElementById("analyse");
+    var message = document.querySelector(".centered-message");
+
+    if (window.getComputedStyle(analyse).display === "none") {
+        analyse.style.display = "block";
+        message.style.display = "none";
+    } else {
+        analyse.style.display = "none";
+        message.style.display = "block";
+    }
+}
